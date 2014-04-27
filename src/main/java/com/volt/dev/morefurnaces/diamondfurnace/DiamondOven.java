3e40cbbcf51af712d9957df52378b4690cd7cd20
Matthew Merhtens <FuturisticalMatthew@gmail.com>
2014-04-27 13:31:57 -0500

    Added new Diamond Textures. Also fixed Creative Tab.

--- src/main/java/com/volt/dev/morefurnaces/diamondfurnace/DiamondOven.java
+++ src/main/java/com/volt/dev/morefurnaces/diamondfurnace/DiamondOven.java
@@ -108,7 +108,7 @@ public class DiamondOven extends BlockContainer
     {
         this.blockIcon = p_149651_1_.registerIcon("MoreFurnaces:DiamondFurnace_side");
         this.field_149936_O = p_149651_1_.registerIcon(this.field_149932_b ? "MoreFurnaces:DiamondFurnace_front_active" : "MoreFurnaces:DiamondFurnace_front_idle");
-        this.field_149935_N = p_149651_1_.registerIcon("MoreFurnaces:DiamondFurnace_side");
+        this.field_149935_N = p_149651_1_.registerIcon("MoreFurnaces:DiamondFurnace_top");
     }
 
     /**
