d409f649ecf7e8bcd470a886e72cdb35caaec7cb
Matthew Merhtens <FuturisticalMatthew@gmail.com>
2014-04-29 07:54:51 -0500

    Changed version to 'ALPHA'

--- src/main/java/com/volt/dev/morefurnaces/misc/References.java
+++ src/main/java/com/volt/dev/morefurnaces/misc/References.java
@@ -3,5 +3,5 @@ package com.volt.dev.morefurnaces.misc;
 public class References {
 	public static final String MODID = "MoreFurnaces";
 	public static final String NAME = "Mo' Furnaces";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "ALPHA";
 }
